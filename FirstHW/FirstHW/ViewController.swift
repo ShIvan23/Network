f7b99f9d95ae1d24b94b17fbe6ae989cd4e437d2
Ivan Shishkin <ivan23-1@yandex.ru>
2020-10-24 19:55:20 +0300

    Add first home work

---
+++ FirstHW/FirstHW/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FirstHW
+//
+//  Created by Ivan on 18.10.2020.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    private let url = URL(string: "https://pngimg.com/uploads/github/github_PNG40.png")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createUI()
+    }
+
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    private func createUI() {
+        imageView.kf.setImage(with: url)
+    }
+}
