721603869c5b75b81d3bf5ce9fbf2d0a86223b70
Ivan Shishkin <ivan23-1@yandex.ru>
2020-11-12 21:20:09 +0300

    Add fourth home work

---
+++ FourthHW/FirstHW/NetworkManager.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkManager.swift
+//  FirstHW
+//
+//  Created by Ivan on 28.10.2020.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static func searchRepositoryWithoutSort(urlResponse: String, repoName: String, language: String, completion: @escaping (_ model: RepositoryModel) -> ()) {
+        let urlForSearch = urlResponse + repoName + "+language:" + language
+        guard let url = URL(string: urlForSearch) else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                print(error!.localizedDescription)
+                return }
+            
+            do {
+                let json = try JSONDecoder().decode(RepositoryModel.self, from: data)
+                completion(json)
+
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
+    
+    static func searchRepositoryWithSort(urlResponse: String, repoName: String, language: String, sort: String, completion: @escaping (_ model: RepositoryModel) -> ()) {
+        let urlForSearch = urlResponse + repoName + "+language:" + language + sort
+        guard let url = URL(string: urlForSearch) else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                print(error!.localizedDescription)
+                return }
+            
+            do {
+                let json = try JSONDecoder().decode(RepositoryModel.self, from: data)
+                completion(json)
+
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
