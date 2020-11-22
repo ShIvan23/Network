4a90143443069f4ab8d5fd1321eaf067522e1522
Ivan Shishkin <ivan23-1@yandex.ru>
2020-11-22 21:04:22 +0300

    Finish fourth home work

---
+++ FourthHW/FirstHW/SearchViewController.swift
@@ -0,0 +1,157 @@
+//
+//  SearchViewController.swift
+//  FirstHW
+//
+//  Created by Ivan on 21.11.2020.
+//
+
+import UIKit
+import Kingfisher
+
+class SearchViewController: UIViewController {
+    
+    //    MARK: - Public Properties
+    var userName: String? {
+        didSet {
+            userNameLabel.text = "Hello, \(userName ?? "")"
+        }
+    }
+    
+    var imageURL: String? {
+        didSet {
+            guard let url = URL(string: imageURL!) else { return }
+            avatarImageView.kf.setImage(with: url)
+        }
+    }
+    
+    //    MARK: - Private Properties
+    private let sort = "1&sort=stars&order=desc"
+    private var response = "https://api.github.com/search/repositories?q="
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 30)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 64
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let repoNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "repository name"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let languageTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "languagee"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        segment.insertSegment(withTitle: "Without sort", at: 0, animated: true)
+        segment.insertSegment(withTitle: "With sort", at: 1, animated: true)
+        segment.selectedSegmentIndex = 0
+        return segment
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Search repository", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return button
+    }()
+    
+    //    MARK: - Life Cicles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createUI()
+    }
+    
+    //    MARK: - Privates Methods
+    private func createUI() {
+        view.backgroundColor = .white
+        
+        searchButton.addTarget(self, action: #selector(tapSearchButtom), for: .touchUpInside)
+        
+        let elements = [userNameLabel, avatarImageView, repoNameTextField, languageTextField, segmentControl, searchButton]
+        elements.forEach { (element) in
+            view.addSubview(element)
+        }
+        
+        let constraints = [
+            userNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            userNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            avatarImageView.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 30),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 128),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 128),
+            avatarImageView.centerXAnchor.constraint(equalTo: userNameLabel.centerXAnchor),
+            
+            repoNameTextField.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 30),
+            repoNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            repoNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            repoNameTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            languageTextField.topAnchor.constraint(equalTo: repoNameTextField.bottomAnchor, constant: 15),
+            languageTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            languageTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            languageTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            segmentControl.topAnchor.constraint(equalTo: languageTextField.bottomAnchor, constant: 35),
+            segmentControl.leadingAnchor.constraint(equalTo: languageTextField.leadingAnchor),
+            segmentControl.trailingAnchor.constraint(equalTo: languageTextField.trailingAnchor),
+            segmentControl.heightAnchor.constraint(equalToConstant: 40),
+            
+            searchButton.topAnchor.constraint(equalTo: segmentControl.bottomAnchor, constant: 45),
+            searchButton.leadingAnchor.constraint(equalTo: segmentControl.leadingAnchor),
+            searchButton.trailingAnchor.constraint(equalTo: segmentControl.trailingAnchor),
+            searchButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @objc private func tapSearchButtom() {
+        guard let nameText = repoNameTextField.text else { return }
+        guard let languageText = languageTextField.text else { return }
+        
+        if segmentControl.selectedSegmentIndex == 0 {
+            NetworkManager.searchRepositoryWithoutSort(urlResponse: response, repoName: nameText, language: languageText) { [weak self] (json) in
+                
+                DispatchQueue.main.async {
+                    let tableVC = SearchTableViewController(json: json)
+                    self?.navigationController?.pushViewController(tableVC, animated: true)
+                }
+            }
+        } else {
+            
+            NetworkManager.searchRepositoryWithSort(urlResponse: response, repoName: nameText, language: languageText, sort: sort) { [weak self] (json) in
+                
+                DispatchQueue.main.async {
+                    let tableVC = SearchTableViewController(json: json)
+                    self?.navigationController?.pushViewController(tableVC, animated: true)
+                }
+            }
+        }
+    }
+}
--- FourthHW/FirstHW/ViewController.swift
+++ FourthHW/FirstHW/ViewController.swift
@@ -18,17 +18,7 @@ class ViewController: UIViewController {
     
     //    MARK: - Private Properties
     private let url = URL(string: "https://pngimg.com/uploads/github/github_PNG40.png")
-    private let authorization: AuthorizationProtocol?
-    
-    //    MARK: - Initializers
-    init(authorization: AuthorizationProtocol = AuthorizationManger()) {
-        self.authorization = authorization
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let authorization: AuthorizationProtocol = AuthorizationManger()
     
     //    MARK: - Life Cycles Methods
     override func viewDidLoad() {
@@ -36,53 +26,26 @@ class ViewController: UIViewController {
         createUI()
     }
     
+    //    MARK: - IBActions
+    @IBAction func loginButton(_ sender: Any) {
+        createRequest()
+    }
+    
     //    MARK: - Private Methods
     private func createUI() {
         imageView.kf.setImage(with: url)
     }
     
-    //    MARK: - IBActions
-    @IBAction func loginButton(_ sender: Any) {
-//        apiRequest()
-        createRequest()
-    }
-    
-    private func apiRequest() {
-        let https = "https://api.github.com/user"
-        let key = "Authorization"
-        let value = "ShIvan23:1edf9ede9a5a11f184d112fb3b8ae070a8b803dd"
-        guard let value64 = value.data(using: .utf8)?.base64EncodedString() else { return }
-        //        let value64 = valueUTF.base64EncodedString()
-        let header = [key: "Basic \(value64)"]
-        guard let url = URL(string: https) else { return }
-        var request = URLRequest(url: url)
-        request.allHTTPHeaderFields = header
-        //        request.setValue("Basic \(value64)", forHTTPHeaderField: key)
-        
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            if let error = error {
-                print(error.localizedDescription)
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                print(response.statusCode)
-            }
-            
-            if let data = data {
-                print(data)
-            }
-        }
-        dataTask.resume()
-    }
-    
     private func createRequest() {
         guard let userName = userNameTextField.text else { return }
         guard let password = passwordTextField.text else { return }
-        authorization?.parseJSON(userName: userName, password: password, completionHandler: { (user) in
-//            Передать в Second VC имя и картинку. Переопределить метод prepare
+        authorization.parseJSON(userName: userName, password: password, completionHandler: { [weak self] (user) in
+            DispatchQueue.main.async {
+                let vc = SearchViewController()
+                vc.userName = user?.login ?? ""
+                vc.imageURL = user?.avatarURL ?? ""
+                self?.navigationController?.pushViewController(vc, animated: true)
+            }
         })
     }
-    
-   
 }
